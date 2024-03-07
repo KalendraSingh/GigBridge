73a9c50cd3c11435a02140890a701a4f9ea779e0
KalendraSingh <sangamslecture@gmail.com>
2024-03-08 01:34:42 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ config({ path: "./config/config.env" });
 //   })
 // );
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: [process.env.FRONTEND_URL],
   credentials: true,
   allowedHeaders: ['X-Requested-With, Content-Type, Authorization'],
   methods: 'GET, POST, PATCH, PUT, POST, DELETE, OPTIONS'
